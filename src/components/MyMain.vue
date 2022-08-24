80acdeeaa2aa9ad54a399c08ef340535e45a0505
Luca Somaschini <somas99@gmail.com>
2022-08-24 18:15:06 +0200

    Main: completed

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>test M</div>
+    <main>
+        --> content goes here
+    </main>
 </template>
 
 <script>
@@ -8,6 +10,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+main {
+    background-color: #1c1c1c;
+    height: 300px;
+    width: 100%;
+    color: white;
+}
 </style>
